0c0dab86c394d983e2aa433411972a0784e7a11f
Mike Bett <mike.bett@student.moringa.school.com>
2025-05-04 09:19:37 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence =[]
+    for i in range (length):
+        if i == 0:
+            sequence.append(0)
+        elif i == 1:
+            sequence.append(1)
+        else:
+            sequence.append(sequence[-1] + sequence[-2]) 
+    print(sequence)
+
+                
     pass
